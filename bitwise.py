4693d2162442a5d760dbd8265b4b1f5cb663cc8f
athallah21 <athallahravi21@gmail.com>
2024-03-02 12:02:53 +0700

    baru

---
+++ bitwise.py
@@ -0,0 +1,28 @@
+# operasi binary 
+
+a = 3
+b = 8
+
+# or
+print("=================OR===============")
+c = a | b
+print( "nilai= ", a ,"binary =", format((a),"08b") )
+print( "nilai= ", b ,"binary =", format((b),"08b") )
+print("------------------------------------|")
+print( "nilai= ", c ,"binary =", format((c),"08b") )
+
+# and
+print("=================AND===============")
+c = a & b
+print( "nilai= ", a ,"binary =", format((a),"08b") )
+print( "nilai= ", b ,"binary =", format((b),"08b") )
+print("------------------------------------&")
+print( "nilai= ", c ,"binary =", format((c),"08b") )
+
+# XOR
+print("=================XOR===============")
+c = a ^ b
+print( "nilai= ", a ,"binary =", format((a),"08b") )
+print( "nilai= ", b ,"binary =", format((b),"08b") )
+print("------------------------------------^")
+print( "nilai= ", c ,"binary =", format((c),"08b") )
